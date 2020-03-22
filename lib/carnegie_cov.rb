daf4a1737e1c4defca55eb0296b2e9d08ac3044b
Marci Clark <marcig612@gmail.com>
2020-03-22 21:07:15 +0000

    first commit

---
+++ lib/carnegie_cov.rb
@@ -0,0 +1,6 @@
+require "carnegie_cov/version"
+
+module CarnegieCov
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/carnegie_cov/version.rb
@@ -0,0 +1,3 @@
+module CarnegieCov
+  VERSION = "0.1.0"
+end
