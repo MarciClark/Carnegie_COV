98a1f3518c3aaafb24bb4cbb9bb61be7d6059d78
Marci Clark <marcig612@gmail.com>
2020-03-23 17:38:52 +0000

    add folders

--- lib/carnegie_cov.rb
+++
@@ -1,6 +0,0 @@
-require "carnegie_cov/version"
-
-module CarnegieCov
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/environment.rb
@@ -0,0 +1,15 @@
+require "carnegie_cov/version"
+require_relative "./carnegie_cov/cli"
+require_relative "./carnegie_cov/gallery"
+require_relative "./carnegie_cov/scraper"
+require_relative "./carnegie_cov/theater"
+
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module CarnegieCov
+  class Error < StandardError; end
+  # Your code goes here...
+end
