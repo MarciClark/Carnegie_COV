3c7b7a69435e5697307e4948c94035f7ef2fa0ca
Marci Clark <marcig612@gmail.com>
2020-03-29 19:18:14 +0000

    add call method

---
+++ lib/carnegie_cov/cli.rb
@@ -0,0 +1,16 @@
+class CarnegieCov::CLI 
+  
+  @@muted="\e[1;31m"
+  @@grn="\e[1;32m"
+  @@blu="\e[1;34m"
+  @@mag="\e[1;35m"
+  @@cyn="\e[1;36m"
+  @@white="\e[0m"
+  
+  def call 
+    list_categories 
+    user_input
+    goodbye
+  end 
+  
+  
